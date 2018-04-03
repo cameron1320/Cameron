b472f26f2985493941ee2fe55be5195efd86621d
cameron1320 <34429865+cameron1320@users.noreply.github.com>
2018-04-02 22:22:40 -0700

    Intro Edits with sarah

--- appendices/code/PolInert.m
+++ appendices/code/PolInert.m
@@ -1,10 +1,1 @@
-function [ Ip ] = PolInert( obj, do,l,rho,di )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-if nargin == 4
-    di = 0;
-end
- Ip = (1/2)*pi*rho*l*((do/2)-(di/2))^2*((do/2)^2 + (di/2)^2);
-
-end
-
+Internal Server Error
